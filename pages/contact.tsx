d3903b61012831d7bd200ffcde3f10253628fd76
ehsanmoodi <ehsanmoodi90@gmail.com>
2022-08-02 02:59:12 +0430

    contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import Link from "next/link";
 import { PageTemplate } from "../templates";
 
 const Contact: NextPage = () => {
@@ -20,7 +21,23 @@ const Contact: NextPage = () => {
         },
       ]}
     >
-      <div></div>
+      <div className="contact-page">
+        <p>
+          خوشحال میشیم بتونیم ملاقاتتون کنیم، اما در حال حاضر امکانش رو
+          نداریم.از طریق ایمیل و چت پشتیبانی می تونید با ما در ارتباط باشید.
+        </p>
+
+        <ul className="contact-page__items">
+          <li>
+            <span>چت پشتیبانی</span>
+            <Link href="tel:۰۹۱۲۰۵۴۸۵۶۲">۰۹۱۲۰۵۴۸۵۶۲</Link>
+          </li>
+          <li>
+            <span>ایمیل پشتیبانی</span>
+            <Link href="mailto:sadezibast@gmail.com">sadezibast@gmail.com</Link>
+          </li>
+        </ul>
+      </div>
     </PageTemplate>
   );
 };
